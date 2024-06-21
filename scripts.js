166f01856d428debf460ef1b7cf0abd84f891050
henriquepdrin <henr9pedro@gmail.com>
2024-06-21 12:13:42 -0300

    2º commit

--- scripts.js
+++ scripts.js
@@ -1,32 +1,57 @@
-let btnNext = document.querySelector('.next')
-let btnBack = document.querySelector('.back')
+let btnNext = document.querySelector('.next');
+let btnBack = document.querySelector('.back');
 
-let container = document.querySelector('.container')
-let list = document.querySelector('.container .list')
-let thumb = document.querySelector('.container .thumb')
+let container = document.querySelector('.container');
+let list = document.querySelector('.container .list');
+let thumb = document.querySelector('.container .thumb');
 
-btnNext.onclick = () => moveItensOnClick('next')
-btnBack.onclick = () => moveItensOnClick('back')
+btnNext.addEventListener('click', () => moveItemsOnClick('next'));
+btnBack.addEventListener('click', () => moveItemsOnClick('back'));
 
+function moveItemsOnClick(type) {
+    let listItems = document.querySelectorAll('.list .list-item');
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item');
 
-function moveItensOnClick (type) {
-        let listItems = document.querySelectorAll('.list .list-item')
-        let thumbItems = document.querySelectorAll('.thumb .thumb-item')
-
-    
-      if(type === 'next'){
-        list.appendChild(listItems[0])
-        thumb.appendChild(thumbItems[0])
-        container.classList.add('next')
-      } else {
-        list.prepend(listItems[listItems.length - 1])
-        thumb.prepend(thumbItems[thumbItems.length -1])
-        container.classList.add('back')
-      }
-
-      setTimeout(() => {
-        container.classList.remove('next')
-        container.classList.remove('back')
-      }, 3000);
-
+    if (type === 'next') {
+        list.appendChild(listItems[0]);
+        thumb.appendChild(thumbItems[0]);
+        container.classList.add('next');
+    } else {
+        list.prepend(listItems[listItems.length - 1]);
+        thumb.prepend(thumbItems[thumbItems.length - 1]);
+        container.classList.add('back');
     }
+
+    setTimeout(() => {
+        container.classList.remove('next');
+        container.classList.remove('back');
+    }, 3000);
+}
+
+// Modal handling function
+function setupModal(modalId, btnId, closeClass) {
+    var modal = document.getElementById(modalId);
+    var btn = document.getElementById(btnId);
+    var span = modal.getElementsByClassName(closeClass)[0];
+
+    btn.onclick = function() {
+        modal.style.display = "flex";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+// Setup modals
+setupModal('myModal', 'myBtn', 'close');
+setupModal('myModal2', 'espbutton', 'close');
+setupModal('myModal3', 'relebutton', 'close');
+setupModal('myModal4', 'diagramabutton', 'close');
+
